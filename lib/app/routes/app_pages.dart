a2612ba059a3d79b425e1b8a44a98f675db938b8
johnson-lgtm <johnson@connectia.in>
2026-04-14 18:40:43 +0530

    Refactor AppPages to remove dynamic settings-based transitions; simplify route definitions

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:pokemonapp/app/binding/detail_binding.dart';
 import 'package:pokemonapp/app/binding/fav_binding.dart';
 import 'package:pokemonapp/app/binding/home_binding.dart';
-import 'package:pokemonapp/app/controller/settings_controller.dart';
 import 'package:pokemonapp/app/views/fav_view.dart';
 import 'package:pokemonapp/app/views/home_wrapper_view.dart';
 import '../views/detail_view.dart';
@@ -10,36 +9,21 @@ import '../views/detail_view.dart';
 part 'app_routes.dart';
 
 class AppPages {
-  static List<GetPage> get pages {
-    final settings = Get.find<SettingsController>();
-    return [
-      GetPage(
-        name: Routes.HOME,
-        page: () => HomeWrapperView(),
-        transition:
-            settings.animationsEnabled.value
-                ? Transition.fade
-                : Transition.noTransition,
-        binding: HomeBinding(),
-      ),
-      GetPage(
-        name: Routes.DETAIL,
-        page: () => DetailView(),
-        transition:
-            settings.animationsEnabled.value
-                ? Transition.fade
-                : Transition.noTransition,
-        binding: DetailBinding(),
-      ),
-      GetPage(
-        name: Routes.FAVOURITE,
-        page: () => FavoritesView(),
-        transition:
-            settings.animationsEnabled.value
-                ? Transition.fade
-                : Transition.noTransition,
-        binding: FavoritesBinding(),
-      ),
-    ];
-  }
+  static final pages = [
+    GetPage(
+      name: Routes.HOME,
+      page: () => HomeWrapperView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: Routes.DETAIL,
+      page: () => DetailView(),
+      binding: DetailBinding(),
+     ),
+        GetPage(
+      name: Routes.FAVOURITE,
+      page: () => FavoritesView(),
+      binding: FavoritesBinding(),
+    )
+  ];
 }
